af3693e39b60c4f987d7d0d2849760518c3e3eaf
Taha Ben Masaud <tbm206@gmail.com>
2025-03-19 19:28:12 +0000

    chore: switch to esbuild

---
+++ shamba/client/esbuild.mjs
@@ -0,0 +1,55 @@
+import civetPlugin from "@danielx/civet/esbuild";
+import * as esbuild from "esbuild";
+import envFilePlugin from "esbuild-envfile-plugin";
+
+const config = {
+  entryPoints: ["./src/main"],
+  jsxFactory: "h",
+  jsxFragment: "Fragment",
+  jsx: "automatic",
+  bundle: true,
+  sourcemap: process.env.NODE_ENV !== "production",
+  minify: process.env.NODE_ENV === "production",
+  metafile: process.env.NODE_ENV !== "production",
+  chunkNames: "[name]",
+  target: "es6",
+  loader: { ".js": "jsx" },
+  alias: { "~": "./src", react: "preact/compat", "react-dom": "preact/compat" },
+  outdir: "../server/static/js",
+  plugins: [
+    envFilePlugin,
+    civetPlugin({
+      // Options and their defaults:
+      // dts: false,                     // generate .d.ts files?
+      // outputExtension: '.civet.tsx',  // replaces .civet in output
+      js: true, // use Civet's TS -> JS transpiler?
+    }),
+  ],
+};
+
+if (process.argv.includes("--watch")) {
+  esbuild
+    .context({ ...config, sourcemap: "both" })
+    .then((ctx) => {
+      ctx.watch();
+      console.log("Watching...");
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+} else {
+  esbuild
+    .build(config)
+    .then((result) => {
+      console.log("⚡ Done");
+
+      if (process.env.NODE_ENG !== "production") {
+        esbuild;
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
