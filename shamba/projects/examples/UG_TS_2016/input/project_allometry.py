50144c2a6557c45c125e1dedd3c4aa616c29e5a0
Helen Hughes <hhughes.92@gmail.com>
2025-11-12 12:07:38 +0000

    Fix typos

--- shamba/projects/examples/UG_TS_2016/input/project_allometry.py
+++ shamba/projects/examples/UG_TS_2016/input/project_allometry.py
@@ -4,8 +4,8 @@
 # a dictionary called "allometric" maps allometric keys to functions
 
 def swietenia_macrophylla(dbh, tree_params):
-    abg = 0.903*((dbh**2)*(0.6488*dbh + 1.7084))**0.684
-    return abg * tree_params.carbon
+    agb = 0.903*((dbh**2)*(0.6488*dbh + 1.7084))**0.684
+    return agb * tree_params.carbon
 
 gmelina_data = {
                     "dbh": [0, 25, 50, 62.5, 75, 87.5],
@@ -16,8 +16,8 @@ def gmelina_arborea(dbh, tree_params):
     try:
         index = gmelina_data['dbh'].index(dbh)
         height = gmelina_data['height'][index]
-        abg = 0.06*(dbh**2 * height)**0.88
-        return abg * tree_params.carbon
+        agb = 0.06*(dbh**2 * height)**0.88
+        return agb * tree_params.carbon
     except ValueError:
         # Handle case where exact dbh is not found in input data
         raise ValueError(f"DBH value {dbh} not found in gmelina_data")
