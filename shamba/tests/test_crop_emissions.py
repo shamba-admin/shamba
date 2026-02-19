6d70dc9d8eddbc5d6be5a47c3fccff02f7d2d23b
Helen Hughes <hhughes.92@gmail.com>
2026-02-19 12:07:42 +0000

    Update test files: 1. parameterise tests 2. include second test scenario 3. update expected test results after recent code changes. All tests pass.

--- shamba/tests/test_crop_emissions.py
+++ shamba/tests/test_crop_emissions.py
@@ -6,9 +6,202 @@ from model.common import csv_handler
 from model import configuration
 from model.crop_model import get_crop_bases, get_crop_projects
 
+#-- Expected emissions arrays -- #
+WL_expected_base_emissions = [0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+        ]
+WL_expected_project_emissions = [0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.22413,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+        ]
 
-def test_crop_model():
-    file_path = os.path.join(configuration.TESTS_DIR, "fixtures", "WL_input.csv")
+testB_expected_base_emissions = [0.0000,
+0.24072,
+0.24072,
+0.24072,
+0.24072,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+0.10581,
+]
+testB_expected_project_emissions = [0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.21471,
+0.19677,
+0.19677,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.11064,
+0.11064,
+0.11064,
+0.11064,
+0.12334,
+0.11064,
+0.11064,
+0.11064,
+0.11064,
+0.11064,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+0.00000,
+]
+
+#-- Test function -- #
+
+@pytest.mark.parametrize("csv_input_file, expected_base_emissions, expected_project_emissions", [
+    pytest.param("WL_input.csv", WL_expected_base_emissions, 
+                WL_expected_project_emissions, id = "Test Case: WL"),
+    pytest.param("testB_input.csv", testB_expected_base_emissions, testB_expected_project_emissions, id = "Test Case: testB"),
+])
+def test_crop_model(csv_input_file, expected_base_emissions, expected_project_emissions): 
+    file_path = os.path.join(configuration.TESTS_DIR, "fixtures", csv_input_file)
     csv_input_data = csv_handler.get_csv_input_data(0, file_path)
     N_YEARS = int(csv_input_data["yrs_proj"])
 
@@ -19,121 +212,38 @@ def test_crop_model():
         input_data=csv_input_data, no_of_years=N_YEARS, start_index=1, end_index=3
     )
 
-    fire_base = np.zeros(N_YEARS)
-    fire_proj = np.zeros(N_YEARS)
+    base_fire_interval = int(csv_input_data["fire_int_base"])
+    if base_fire_interval == 0:
+        fire_base = np.zeros(N_YEARS)
+    else:
+        fire_base = np.zeros(N_YEARS)
+        fire_base[::base_fire_interval] = int(csv_input_data["fire_pres_base"])
+
+    proj_fire_interval = int(csv_input_data["fire_int_proj"])
+    if proj_fire_interval == 0:
+        fire_project = np.zeros(N_YEARS)
+    else:
+        fire_project = np.zeros(N_YEARS)
+        fire_project[::proj_fire_interval] = int(csv_input_data["fire_pres_proj"])
+
+    base_fire_off_field = int(csv_input_data["fire_off_base"])
+    if base_fire_off_field == 1:
+        burn_off_base = True
+    else:        burn_off_base = False
+
+    proj_fire_off_field = int(csv_input_data["fire_off_proj"])
+    if proj_fire_off_field == 1:
+        burn_off_project = True
+    else:        burn_off_project = False
 
     crop_base_emissions = Emit.create(
-        no_of_years=N_YEARS, crop=crop_base, fire=fire_base
-    )
+        no_of_years=N_YEARS, crop=crop_base, fire=fire_base, burn_off=burn_off_base)
     crop_project_emissions = Emit.create(
-        no_of_years=N_YEARS, crop=crop_project, fire=fire_proj
+        no_of_years=N_YEARS, crop=crop_project, fire=fire_project, burn_off=burn_off_project
     )
-
     assert crop_base_emissions == pytest.approx(
-        [
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-            0.22412858,
-        ]
+        expected_base_emissions, rel=1e-4
     )
     assert crop_project_emissions == pytest.approx(
-        [
-            0.19579005,
-            0.19579005,
-            0.19579005,
-            0.19579005,
-            0.19579005,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-            0.07945792,
-        ]
+        expected_project_emissions, rel=1e-4
     )
--- shamba/tests/test_fertiliser_emissions.py
+++ shamba/tests/test_fertiliser_emissions.py
@@ -6,9 +6,205 @@ from model.common import csv_handler
 from model import configuration
 import model.litter as LitterModel
 
+#-- Expected emissions arrays -- #
+WL_expected_base_emissions = [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ]
+WL_expected_project_emissions = [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ]
 
-def test_fertiliser_model():
-    file_path = os.path.join(configuration.TESTS_DIR, "fixtures", "WL_input.csv")
+testB_expected_base_emissions = [0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+]
+testB_expected_project_emissions = [0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+0.0000,
+0.0000,
+0.7636,
+]
+
+#-- Test function -- #
+
+@pytest.mark.parametrize("csv_input_file, expected_base_emissions, expected_project_emissions", [
+    pytest.param("WL_input.csv", WL_expected_base_emissions, 
+                WL_expected_project_emissions, id = "Test Case: WL"),
+    pytest.param("testB_input.csv", testB_expected_base_emissions, testB_expected_project_emissions, id = "Test Case: testB"),
+])
+
+def test_fertiliser_model(csv_input_file, expected_base_emissions, expected_project_emissions):
+    file_path = os.path.join(configuration.TESTS_DIR, "fixtures", csv_input_file)
     csv_input_data = csv_handler.get_csv_input_data(0, file_path)
     N_YEARS = int(csv_input_data["yrs_proj"])
 
@@ -24,120 +220,40 @@ def test_fertiliser_model():
         nitrogen=float(csv_input_data["proj_sf_n"]),
         no_of_years=N_YEARS,
     )
+    base_fire_interval = int(csv_input_data["fire_int_base"])
+    if base_fire_interval == 0:
+        fire_base = np.zeros(N_YEARS)
+    else:
+        fire_base = np.zeros(N_YEARS)
+        fire_base[::base_fire_interval] = int(csv_input_data["fire_pres_base"])
+
+    proj_fire_interval = int(csv_input_data["fire_int_proj"])
+    if proj_fire_interval == 0:
+        fire_project = np.zeros(N_YEARS)
+    else:
+        fire_project = np.zeros(N_YEARS)
+        fire_project[::proj_fire_interval] = int(csv_input_data["fire_pres_proj"])
+
+    base_fire_off_field = int(csv_input_data["fire_off_base"])
+    if base_fire_off_field == 1:
+        burn_off_base = True
+    else:        burn_off_base = False
+
+    proj_fire_off_field = int(csv_input_data["fire_off_proj"])
+    if proj_fire_off_field == 1:
+        burn_off_project = True
+    else:        burn_off_project = False
 
     fertiliser_base_emissions = Emit.create(
-        no_of_years=N_YEARS, fert=[synthetic_fertiliser_base]
+        no_of_years=N_YEARS, fert=[synthetic_fertiliser_base], fire=fire_base, burn_off=burn_off_base
     )
     fertiliser_project_emissions = Emit.create(
-        no_of_years=N_YEARS, fert=[synthetic_fertiliser_project]
+        no_of_years=N_YEARS, fert=[synthetic_fertiliser_project], fire=fire_project, burn_off=burn_off_project
     )
 
     assert fertiliser_base_emissions == pytest.approx(
-        [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-        ]
-    )
+        expected_base_emissions, rel=1e-4)
 
     assert fertiliser_project_emissions == pytest.approx(
-        [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-        ]
-    )
+        expected_project_emissions, rel=1e-4)
+
